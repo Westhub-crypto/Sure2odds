0fba57a0bdc266a06d4e718838c7ec67e9055370
Westhub-crypto <godwinoloja4@gmail.com>
2026-05-06 09:50:23 +0100

    Refactor server.js for improved structure and routes

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
-require("dotenv").config();
-const express = require("express");
-const connectDB = require("./config/db");
+require('dotenv').config();
+const express = require('express');
+const connectDB = require('./config/db');
+const bot = require('./bot');
+const webhookRoutes = require('./routes/webhookRoutes');
+const { startCronJobs } = require('./jobs/cronJobs');
 
 const app = express();
 app.use(express.json());
 
 connectDB();
+startCronJobs(bot);
 
-app.use("/api", require("./routes/webhookRoutes"));
+app.use('/webhook', webhookRoutes);
 
-require("./jobs/cronJobs");
+app.get('/', (req, res) => res.send('Sure 2 Odds Ultra is running securely!'));
 
-app.listen(3000, () => console.log("Server running"));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
