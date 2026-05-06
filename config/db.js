ca4d9534083f163e21f4d82db71d6fec7281a097
Westhub-crypto <godwinoloja4@gmail.com>
2026-05-06 09:52:25 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,6 +1,13 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-module.exports = async () => {
-  await mongoose.connect(process.env.MONGO_URI);
-  console.log("MongoDB Connected");
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('✅ MongoDB Connected');
+    } catch (error) {
+        console.error('❌ MongoDB Connection Error:', error);
+        process.exit(1);
+    }
 };
+
+module.exports = connectDB;
