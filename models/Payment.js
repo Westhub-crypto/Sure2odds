860c39604f14874ee5158dc749e041b34c9b020d
Westhub-crypto <godwinoloja4@gmail.com>
2026-05-06 09:55:47 +0100

    Update Payment.js

--- models/Payment.js
+++ models/Payment.js
@@ -1,12 +1,13 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const schema = new mongoose.Schema({
-  userId: mongoose.Schema.Types.ObjectId,
-  amount: Number,
-  method: String,
-  status: String,
-  reference: String,
-  receipt: String
-});
+const paymentSchema = new mongoose.Schema({
+    telegramId: String,
+    amount: Number,
+    currency: String,
+    method: { type: String, enum: ['squadco', 'manual'] },
+    status: { type: String, enum: ['pending', 'approved', 'rejected'], default: 'pending' },
+    receiptImageId: String,
+    reference: String
+}, { timestamps: true });
 
-module.exports = mongoose.model("Payment", schema);
+module.exports = mongoose.model('Payment', paymentSchema);
